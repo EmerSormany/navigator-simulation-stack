2f64d8011759e9f4e6ff4c3fe303775257fc5f3e
EmerSormany <sormanyemerson@gmail.com>
2025-11-29 01:04:29 -0300

    feat: styling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const { routes } = require('./routes/routes')
 const dotenv = require('dotenv')
+const path = require('path')
 
 dotenv.config()
 const app = express()
 
 app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, '../public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(routes)
 
